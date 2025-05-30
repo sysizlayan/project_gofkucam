109f0b7cbf5f8a3ff0edb7eda26f6fb2a10b3a17
sysizlayan <sysizlayan@gmail.com>
2025-05-31 00:18:11 +0200

    [FIX]compilation issues

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # build.sh - Build script for GofkuCam C++ project using CMake
 set -e
 
+sudo apt install -y libspdlog-dev
 BUILD_DIR=build
 
 # Create build directory if it doesn't exist
@@ -9,10 +10,9 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 # Configure project with CMake and export compile_commands.json
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ..
 
 # Build the project
-cmake --build .
+make -j4
 
 cd ..
-echo "Build successful. Binaries are in $BUILD_DIR."
--- setup_pytorch.sh
+++ setup_pytorch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install -y wget unzip cmake
 cd extern
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
