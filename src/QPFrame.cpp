2b651523faedb87ac18ccdb42e7f6bd9dc67154a
sysizlayan <sysizlayan@gmail.com>
2025-10-09 07:50:49 +0200

    [CHANGE]QP Framework is updated.

--- src/QPFrame.cpp
+++ src/QPFrame.cpp
@@ -1,4 +1,4 @@
-#include "qpcpp.hpp"
+#include "QPFrame.hpp"
 #include <iostream>
 
 #include "CapturingFramesFromVideoStream.hpp"
@@ -73,19 +73,21 @@ void QPFrame::ao_thread_func()
 /*--------------------------------------------------------------------------*/
 void QP::QF::onStartup()
 {
-    QF_setTickRate(1000, 50); // desired tick rate/prio -> 1000 ticks per second!
+    setTickRate(1000, 50); // desired tick rate/prio -> 1000 ticks per second!
 }
-/*--------------------------------------------------------------------------*/
 
+/*--------------------------------------------------------------------------*/
 void QP::QF::onCleanup()
 {
 }
-/*--------------------------------------------------------------------------*/
 
-void QP::QF_onClockTick()
+//............................................................................
+void QP::QF::onClockTick()
 {
-   // process time events at rate 0
-   QP::QF::TICK_X(0U, 0);
+   QTimeEvt::TICK_X(0U, 0); // process time events at rate 0
+   //  QS_RX_INPUT(); // handle the QS-RX input
+   //  QS_OUTPUT();   // handle the QS output
+
 }
 
 /*--------------------------------------------------------------------------*/
