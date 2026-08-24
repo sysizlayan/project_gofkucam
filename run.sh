9545de1021c285c83f93b13adf7a649d2faebeb9
sysizlayan <sysizlayan@gmail.com>
2026-08-24 21:28:51 +0200

    [UPDATE]

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 # run.sh - Build and run the GofkuCam executable
 set -e
 
-# Build the project
-./build.sh
-
 # Run the executable from the build directory if it exists there, otherwise from project root
 if [ -f build/src/gofkucam ]; then
     echo "Running build/gofkucam..."
